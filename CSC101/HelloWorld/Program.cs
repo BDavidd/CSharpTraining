185db2dfef5306ee38d7169ebdb0c8f45ea8fdc8
David Barzi <barzi.david@gmail.com>
2017-09-24 17:15:38 +0300

    Added comments. Added my answers to the flash quiz.

--- CSC101/HelloWorld/Program.cs
+++ CSC101/HelloWorld/Program.cs
@@ -4,6 +4,7 @@ namespace HelloWorld
 {
     class Program
     {
+        /// <summary> Print a string to the console. </summary>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
