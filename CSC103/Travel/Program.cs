f922c4aa10746345263516ff424956cd3a3559bd
David Barzi <barzi.david@gmail.com>
2017-09-25 23:29:20 +0300

    Completed the first exercise.

---
+++ CSC103/Travel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
