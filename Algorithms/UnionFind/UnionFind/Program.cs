6a68bec44635abdb271e7575e97fb3478e639260
David Barzi <barzi.david@gmail.com>
2019-09-08 18:45:28 +0300

    Intro to delegates

---
+++ Algorithms/UnionFind/UnionFind/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnionFind
+{
+    class UF
+    {
+        public UF(int N)
+        {
+
+        }
+
+        public void union(int p, int q)
+        {
+
+        }
+
+        public bool connected(int p, int q)
+        {
+            return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            UF uf = new UF(N);
+
+            string input = Console.ReadLine();
+            int[] results = Array.ConvertAll(input.Split(' '), int.Parse);
+
+            for (int i = 0; i < results.Length - 1; i += 2)
+            {
+                int p = results[i];
+                int q = results[i + 1];
+                if (!uf.connected(p, q))
+                {
+                    uf.union(p, q);
+                    Console.WriteLine($"{p} {q}");
+                }
+            }
+        }
+    }
+}
---
+++ C#/Pluralsight/Delegates/Delegates/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Delegates
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
+    public enum WorkType
+    {
+        Analyze,
+        Design,
+        Build
+    }
+
+    class Worker
+    {
+        public Worker()
+        {
+            WorkPerformedHandler del1 = WorkPerformed1;
+            del1 += WorkPerformed2;
+            WorkPerformedHandler del2 = WorkPerformed2;
+
+            DoWork(del1);
+            del2(3, WorkType.Design);
+        }
+
+        private void DoWork(WorkPerformedHandler del)
+        {
+            del(10, WorkType.Analyze);
+        }
+
+        private void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed1 called: {hours}, {workType}");
+        }
+
+        private void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed2 called: {hours}, {workType}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // ReSharper disable once AssignmentIsFullyDiscarded
+            _ = new Worker();
+        }
+    }
+}
